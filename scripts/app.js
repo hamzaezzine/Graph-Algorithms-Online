0a369346219a867425e1c19b31ac431be0b2076a
hamzaezzine <ezzinehamza070@gmail.com>
2023-05-25 23:56:47 +0100

    kruskal algo

--- scripts/app.js
+++ scripts/app.js
@@ -55,47 +55,207 @@ function parcoursProfondeur(G, d, visited) {
 
 
 // Algorithme glouton de coloriage d'un graphe:
-function coloriageGlouton(G) {
-
-
-}
-
 function welshPowell(G) {
+    var degrees = {};
+    for (var node in G) {
+        if (G.hasOwnProperty(node)) {
+            degrees[node] = G[node].length;
+        }
+    }
 
+    var sortedNodes = Object.keys(degrees).sort(function (a, b) {
+        return degrees[b] - degrees[a];
+    });
+
+    var colors = {};
+    var colorCount = 1;
+
+    for (var i = 0; i < sortedNodes.length; i++) {
+        var node = sortedNodes[i];
+        if (!colors[node]) {
+            colors[node] = colorCount;
+
+            for (var j = i + 1; j < sortedNodes.length; j++) {
+                var adjacent = false;
+                for (var k = 0; k < G[sortedNodes[j]].length; k++) {
+                    if (G[sortedNodes[j]][k] === node) {
+                        adjacent = true;
+                        break;
+                    }
+                }
+
+                if (!adjacent) {
+                    colors[sortedNodes[j]] = colorCount;
+                }
+            }
+        colorCount++;
+        }
+    }
+    return colors;
 }
 
+
+// Algorithme de Welsh-Powell pour colorier un graphe
+function welshPowell(G) {
+    var degrees = {};
+    for (var node in G) {
+        if (G.hasOwnProperty(node)) {
+            degrees[node] = G[node].length;
+        }
+    }
+
+    var sortedNodes = Object.keys(degrees).sort(function (a, b) {
+        return degrees[b] - degrees[a];
+    });
+
+    var colors = {};
+    var colorCount = 0;
+    for (var i = 0; i < sortedNodes.length; i++) {
+        var node = sortedNodes[i];
+        if (!colors[node]) {
+            colorCount++;
+            colors[node] = colorCount;
+            
+            for (var j = i + 1; j < sortedNodes.length; j++){
+                var adjacent = false;
+                for (var k = 0; k < G[sortedNodes[j]].length; k++) {
+                    if (G[sortedNodes[j]][k] === node) {
+                        adjacent = true;
+                        break;
+                    }
+                }
+                if(!adjacent) {
+                    colors[sortedNodes[j]] = colorCount;
+                }
+            }
+        }
+    }
+
+    return colors;
+}
+
+
+
+
+// Algorithme de Prim
 function prim(graph, startNode) {
     var visited = [];
     visited.push(startNode);
     var tree = [];
     var edges = [];
-    
+
     while (visited.length !== Object.keys(graph).length) {
         var minWeight = Infinity;
         var minNode = null;
         var minEdge = null;
-        
 
         for (var i = 0; i < visited.length; i++) {
             if (graph[visited[i]]) {
                 var neighbors = graph[visited[i]];
-                for (var j = 0; j < neighbors.length; j++) {
-                    if (visited.indexOf(neighbors[j].node) === -1 && neighbors[j].weight < minWeight) {
-                        minWeight = neighbors[j].weight;
-                        minNode = neighbors[j].node;
-                        minEdge = {from: visited[i], to: minNode};
+                for (var neighbor in neighbors) {
+                    if (!visited.includes(neighbor) && neighbors[neighbor] < minWeight) {
+                        minWeight = neighbors[neighbor];
+                        minNode = neighbor;
+                        minEdge = { from: visited[i], to: minNode, weight: minWeight };
                     }
                 }
             }
         }
-    
+
         visited.push(minNode);
         tree.push(minEdge);
         edges.push(minEdge);
     }
+
     return edges;
 }
 
+// Algorithme de Kruskal
+function kruskal(graph) {
+    var edges = [];
+    for (var node in graph) {
+        if (graph.hasOwnProperty(node)) {
+            var neighbors = graph[node];
+            for (var neighbor in neighbors) {
+                if (neighbors.hasOwnProperty(neighbor)) {
+                    edges.push({ from: node, to: neighbor, weight: neighbors[neighbor] });
+                }
+            }
+        }
+    }
+    edges.sort(function (a, b) {
+        return a.weight - b.weight;
+    });
+
+    var disjointSet = {};
+    for (var node in graph) {
+        if (graph.hasOwnProperty(node)) {
+            disjointSet[node] = [node];
+        }
+    }
+    var result = [];
+    for (var i = 0; i < edges.length; i++) {
+        var edge = edges[i];
+        var fromSet = disjointSet[edge.from];
+        var toSet = disjointSet[edge.to];
+
+        if (fromSet !== toSet) {
+            result.push(edge);
+            for (var j = 0; j < toSet.length; j++) {
+                fromSet.push(toSet[j]);
+                disjointSet[toSet[j]] = fromSet;
+            }
+        }
+    }
+    return result;
+}
+
+// Algorithme de Bellman-Ford :
+function bellmanFord(graph, startNode) {
+    var distances = {};
+    var predecessors = {};
+
+    for (var node in graph) {
+        distances[node] = Infinity;
+        predecessors[node] = null;
+    }
+    distances[startNode] = 0;
+
+    for (var i = 0; i < Object.keys(graph).length - 1; i++) {
+        for (var node in graph) {
+            if (graph.hasOwnProperty(node)) {
+                var neighbors = graph[node];
+                for (var neighbor in neighbors) {
+                    if (neighbors.hasOwnProperty(neighbor)) {
+                        var weight = neighbors[neighbor];
+                        if (distances[node] + weight < distances[neighbor]) {
+                            distances[neighbor] = distances[node] + weight;
+                            predecessors[neighbor] = node;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (var node in graph) {
+        if (graph.hasOwnProperty(node)) {
+            var neighbors = graph[node];
+            for (var neighbor in neighbors) {
+                if (neighbors.hasOwnProperty(neighbor)) {
+                    var weight = neighbors[neighbor];
+                    if (distances[node] + weight < distances[neighbor]) {
+                        throw new Error("Graph contains negative-weight cycles.");
+                    }
+                }
+            }
+        }
+    }
+
+    return { distances, predecessors };
+}
+
+// Algorithme de Bellman :
 
 
 function createResultList(resultContainer, resultTitle, result) {
@@ -104,13 +264,14 @@ function createResultList(resultContainer, resultTitle, result) {
     resultContainer.appendChild(resultHeading);
 
     var resultList = document.createElement("ul");
-    result.forEach(function (node) {
+    result.forEach(function (node, index) {
         var listItem = document.createElement("li");
-        listItem.textContent = node;
+        listItem.textContent = `${index + 1}. ${node}`;
         resultList.appendChild(listItem);
     });
     resultContainer.appendChild(resultList);
 }
+
 
 function processGraph() {
     var graphInput = document.getElementById("graphInput");
@@ -156,7 +317,7 @@ function processGraph() {
     } 
     else if (selectedAlgorithm === "coloriageGlouton") {
         var colorResult = coloriageGlouton(graph);
-        createResultList(resultContainer, "Coloriage glouton du graphe :", Object.entries(colorResult).map(entry => `${entry[0]} - ${entry[1]}`));
+        createResultList(resultContainer, "Coloriage glouton du graphe :", Object.keys(colorResult).map(key => `${key} - ${colorResult[key]}`));
     }
     else if (selectedAlgorithm === "welshPowell") {
         var colorResult = welshPowell(graph);
@@ -165,7 +326,27 @@ function processGraph() {
     else if (selectedAlgorithm === "prim") {
         var startNode = prompt("Veuillez saisir le sommet de départ pour l'algorithme de Prim :");
         var result = prim(graph, startNode);
-        createResultList(resultContainer, "Arbre couvrant minimal trouvé avec l'algorithme de Prim :", result);
+        createResultList(resultContainer, "Arbre couvrant minimal trouvé avec l'algorithme de Prim :", result.map(edge => `${edge.from} -> ${edge.to} (Poids : ${edge.weight})`));
+    }
+    else if (selectedAlgorithm === "kruskal") {
+        var result = kruskal(graph);
+        createResultList(resultContainer, "Arbre couvrant minimal trouvé avec l'algorithme de Kruskal :", result.map(edge => `${edge.from} -> ${edge.to} (Poids : ${edge.weight})`));
+    }
+    else if (selectedAlgorithm === "bellmanFord") {
+        var startNode = prompt("Veuillez saisir le sommet de départ pour l'algorithme de Bellman-Ford :");
+        try {
+            var result = bellmanFord(graph, startNode);
+            var distances = result.distances;
+            var predecessors = result.predecessors;
+    
+            var distancesList = Object.entries(distances).map(([node, distance]) => `${node}: ${distance}`);
+            var predecessorsList = Object.entries(predecessors).map(([node, predecessor]) => `${node}: ${predecessor || '-'}`);
+    
+            createResultList(resultContainer, "Distances depuis la sommet de départ :", distancesList);
+            createResultList(resultContainer, "Les sommets prédécesseur :", predecessorsList);
+        } catch (error) {
+            alert(error.message);
+        }
     }
     else {
         alert("Algorithme sélectionné non pris en charge.");
