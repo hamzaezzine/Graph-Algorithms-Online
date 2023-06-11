5c5f725687d446cb522a4e211d98117c41199f5e
hamzaezzine <ezzinehamza070@gmail.com>
2023-06-11 21:01:46 +0100

    bellman ford & dijkstra

--- scripts/app.js
+++ scripts/app.js
@@ -37,7 +37,6 @@ function parcoursLargeur(G, d) {
     return fileSommets;
 }
 
-
 // Parcours en profondeur:
 function parcoursProfondeur(G, d, pileSommets) {
     pileSommets.push(d);
@@ -156,6 +155,89 @@ function kruskal(graph) {
 
 
 
+function bellmanFord(graph, debutSommet) {
+    let sommets = Object.keys(graph).length;
+
+    let distances = {};
+    let predecesseurs = {};
+
+    // Initialise les distances et les prédécesseurs
+    for (let sommet in graph) {
+        distances[sommet] = Number.MAX_VALUE;
+        predecesseurs[sommet] = null;
+    }
+    distances[debutSommet] = 0;
+
+    for (let i = 0; i < sommets - 1; i++) {
+        for (let sommet in graph) {
+            for (let voisin in graph[sommet]) {
+                let poids = graph[sommet][voisin];
+                if (distances[sommet] !== Number.MAX_VALUE && distances[sommet] + poids < distances[voisin]) {
+                    distances[voisin] = distances[sommet] + poids;
+                    predecesseurs[voisin] = sommet;
+                }
+            }
+        }
+    }
+
+    // Vérifie les cycles de poids négatifs
+    for (let sommet in graph) {
+        for (let voisin in graph[sommet]) {
+            let poids = graph[sommet][voisin];
+            if (distances[sommet] !== Number.MAX_VALUE && distances[sommet] + poids < distances[voisin]) {
+                console.log("Le graphe contient un cycle de poids négatif");
+                return;
+            }
+        }
+    }
+
+    return {
+        distances: distances,
+        predecesseurs: predecesseurs
+    };
+}
+
+function dijkstra(graph, source) {
+    const sommets = Object.keys(graph);
+    const distances = {};
+    const predecesseurs = {};
+
+    // Initialization
+    sommets.forEach(sommet => {
+        distances[sommet] = Infinity;
+        predecesseurs[sommet] = null;
+    });
+    distances[source] = 0;
+
+    const nonviste = new Set(sommets);
+
+    while (nonviste.size > 0) {
+        let minDistanceSommet = null;
+        for (let sommet of nonviste) {
+            if (minDistanceSommet === null || distances[sommet] < distances[minDistanceSommet]) {
+                minDistanceSommet = sommet;
+            }
+        }
+
+        nonviste.delete(minDistanceSommet);
+
+        for (let voisin in graph[minDistanceSommet]) {
+            const poids = graph[minDistanceSommet][voisin];
+            const totalDistance = distances[minDistanceSommet] + poids;
+
+            if (totalDistance < distances[voisin]) {
+                distances[voisin] = totalDistance;
+                predecesseurs[voisin] = minDistanceSommet;
+            }
+        }
+    }
+
+    return {
+        distances: distances,
+        predecesseurs: predecesseurs
+    };
+}
+
 
 
 
@@ -165,9 +247,9 @@ function createResultList(resultContainer, resultTitle, result) {
     resultContainer.appendChild(resultHeading);
 
     var resultList = document.createElement("ul");
-    result.forEach(function (node, index) {
+    result.forEach(function (sommet, index) {
         var listItem = document.createElement("li");
-        listItem.textContent = `${index + 1}. ${node}`;
+        listItem.textContent = `${index + 1}. ${sommet}`;
         resultList.appendChild(listItem);
     });
     resultContainer.appendChild(resultList);
@@ -221,7 +303,40 @@ function processGraph() {
         var result = kruskal(graph);
         createResultList(resultContainer, "Arbre couvrant minimal trouvé avec l'algorithme de Kruskal :", result.map(arete => `${arete.u} -> ${arete.v} (Poids : ${arete.poids})`));
     }
+    else if (selectedAlgorithm === "bellmanFord") {
+        var debutSommet = prompt("Veuillez saisir le sommet de départ pour l'algorithme de Bellman-Ford :");
+        try {
+            var result = bellmanFord(graph, debutSommet);
+            var distances = result.distances;
+            var predecesseurs = result.predecesseurs;
     
+            var distancesList = Object.entries(distances).map(([sommet, distance]) => `${sommet}: ${distance}`);
+            var predecesseursList = Object.entries(predecesseurs).map(([sommet, predecessor]) => `${sommet}: ${predecessor || '-'}`);
+    
+            createResultList(resultContainer, "Distances depuis la sommet de départ :", distancesList);
+            createResultList(resultContainer, "Les sommets prédécesseur :", predecesseursList);
+        } 
+        catch (error) {
+            alert(error.message);
+        }
+    }
+    else if (selectedAlgorithm === "dijkstra") {
+        var debutSommet = prompt("Veuillez saisir le sommet de départ pour l'algorithme de Dijkstra :");
+        try {
+            var result = dijkstra(graph, debutSommet);
+            var distances = result.distances;
+            var predecesseurs = result.predecesseurs;
+            
+            var distancesList = Object.entries(distances).map(([sommet, distance]) => `${sommet}: ${distance}`);
+            var predecesseursList = Object.entries(predecesseurs).map(([sommet, predecessor]) => `${sommet}: ${predecessor || '-'}`);
+            
+            createResultList(resultContainer, "Distances depuis le sommet de départ :", distancesList);
+            createResultList(resultContainer, "Prédécesseurs des sommets :", predecesseursList);
+        } 
+        catch (error) {
+            alert(error.message);
+        }
+    }
     else {
         alert("Algorithme sélectionné non pris en charge.");
     }
